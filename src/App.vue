9fa956b1008c1f21f97a6f6e24bf8dad5aff4b99
RUUKU SEKIGUCHI <ruukure0827.1027@docomo.ne.jp>
2022-08-18 19:12:28 +0900

    todo完成

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <template>
   <div id="app">
     <h2>Todo List</h2>
-      <input type="text" v-model="item" v-on:keydown.enter="onKeyDown" />
-      <button @click="additem">登録</button>
-    <p>{{ $data }}</p>
+      <div id="form">
+        <input type="text" v-model="item" v-on:keydown.enter="onKeyDown" />
+        <button @click="additem">登録</button>
+      </div>
+    <div class="main">
+      <ul>
+        <li v-for="(todo, index) in todos" :key="`listitem${index}`">
+          <input type="checkbox" v-model="todo.isDone">
+          <p>{{todo.newitem}}</p>
+          <button @click="change()">変更</button>
+            <input type="text" v-show="view" v-model="todo.newitem">
+          <button @click="deleteItem(index)">削除</button>
+        </li> 
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -15,30 +27,58 @@ export default {
   data() {
     return {
       item: "",
+      view:false,
       todos: [],
+
     };
   },
   //処理内容
   methods: {
     onKeyDown() {
-      // console.log(e.key)
       this.additem();
-      },
+    },
     additem() {
       if (this.item == "") {
         alert("未入力")
         return;
       }
-      this.todos.push(this.item);//配列名、Pushの中身は新しく入れる変数など
+      var todo = {
+        newitem: this.item
+      };
+      this.todos.push(todo);
       this.item = "";
     },
+    deleteItem(index){ //indexを引数に指定
+    this.todos.splice(index,1) //indexで指定された要素を1つ削除
+    },
+    change(){
+      this.view = !this.view;
+    },
   },
+
+  // name:"change",
+  // methods: {
+
+  // }
 };
 </script>
 
 <style>
-#app {
+#app h2{
   text-align: center;
 }
 
+#form{
+  text-align: center;
+}
+
+.main {
+  display: flex;
+  justify-content: center;
+}
+
+.main li{
+  text-align:left;
+  list-style: none;
+}
 </style>
